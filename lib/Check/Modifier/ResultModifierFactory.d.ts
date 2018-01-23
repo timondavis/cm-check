49cffcf9da860102498f2a7ff0189fdcd9f8a52e
Timon Davis <timondavis@yahoo.com>
2018-01-23 15:50:50 -0800

    [-] Deleting unused files after refactor

--- lib/Check/Modifier/ResultModifierFactory.d.ts
+++
@@ -1,7 +0,0 @@
-import { ModifierFactory } from "./ModifierFactory";
-import { Modifier } from "./Modifier";
-export declare class ResultModifierFactory extends ModifierFactory {
-    create(name: string, args?: {
-        [key: string]: string;
-    }): Modifier;
-}
