af826c787645b15a7c2e9997a9fa430be2c18cc1
Timon Davis <timondavis@yahoo.com>
2018-01-27 17:16:19 -0800

    [+] Adding tests for TargetModifer

--- src/Test/Modifier/TargetModifier.ts
+++ src/Test/Modifier/TargetModifier.ts
@@ -3,9 +3,9 @@ import { expect } from "chai";
 import { CheckExecutor } from "../../Check/CheckExecutor";
 import { Check } from "../../Check/Check";
 import { TestCore } from "../TestCore";
-import { ResultModifier } from "../../Check/Modifier/ResultModifier";
+import { TargetModifier } from "../../Check/Modifier/TargetModifier";
 
-describe( 'ResultModifier', () => {
+describe( 'TargetModifier', () => {
 
     let CE = CheckExecutor.getInstance();
     let c : Check;
@@ -18,16 +18,17 @@ describe( 'ResultModifier', () => {
     it( 'affects the result of checks', () => {
 
         let modification = TestCore.randomInt();
+        let initialTarget = TestCore.randomInt();
 
-        c = new MyCheck( TestCore.randomInt() );
-        c.addModifier( new ResultModifier( 'Name', modification ));
+        c = new MyCheck( initialTarget );
+        c.addModifier( new TargetModifier( 'Name', modification ));
         CE.execute( c );
-        expect( c.getResult() ).to.be.equal( modification );
+        expect( c.getTarget() ).to.be.equal( initialTarget + modification );
 
-        c = new MyCheck( TestCore.randomInt() );
-        c.addModifier( new ResultModifier( 'Name', modification * - 1 ));
+        c = new MyCheck( initialTarget );
+        c.addModifier( new TargetModifier( 'Name', modification * - 1 ));
         CE.execute( c );
-        expect( c.getResult() ).to.be.equal( modification * -1 );
+        expect( c.getTarget() ).to.be.equal( initialTarget + ( modification * -1 ));
     });
 
     it( 'can process a single or array of numeric string or numeric values', () => {
@@ -44,20 +45,22 @@ describe( 'ResultModifier', () => {
         let modificationSum = modification1 + modification2 + modification3 + modification4 +
             modification5 + modification6 + modification7 + modification8;
 
-        let m1 = new ResultModifier( 'Single-Number', modification1 );
-        let m2 = new ResultModifier( 'Single-String', String( modification5 ) );
-        let m3 = new ResultModifier( 'Multi-Number', [
+        let m1 = new TargetModifier( 'Single-Number', modification1 );
+        let m2 = new TargetModifier( 'Single-String', String( modification5 ) );
+        let m3 = new TargetModifier( 'Multi-Number', [
             modification2,
             modification6
         ]);
-        let m4 = new ResultModifier( 'Multi-String', [
-           String( modification3 ),
-           String( modification4 ),
-           String( modification7 ),
-           String( modification8 )
+        let m4 = new TargetModifier( 'Multi-String', [
+            String( modification3 ),
+            String( modification4 ),
+            String( modification7 ),
+            String( modification8 )
         ]);
 
-        c = new MyCheck( TestCore.randomInt() );
+        let initialTarget = TestCore.randomInt();
+
+        c = new MyCheck( initialTarget );
         c.addModifier( m1 );
         c.addModifier( m2 );
         c.addModifier( m3 );
@@ -65,12 +68,12 @@ describe( 'ResultModifier', () => {
 
         CE.execute( c );
 
-        expect( c.getResult() ).to.be.equal( modificationSum );
+        expect( c.getTarget() ).to.be.equal( initialTarget + modificationSum );
     });
 
     it( 'should report its typename', () => {
 
-        let modifier = new ResultModifier( 'Name', TestCore.randomInt() );
-        expect( modifier.getType() ).to.be.equal( 'result' );
+        let modifier = new TargetModifier( 'Name', TestCore.randomInt() );
+        expect( modifier.getType() ).to.be.equal( 'target' );
     });
 });
