e0943ee99919499df383199015d65ca65ffe0c05
Timon Davis <timondavis@yahoo.com>
2018-01-28 13:28:20 -0800

    [+] Moving index and package around a bit to prepare for publishing

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,4 @@
-import { D20AttributeCheck } from './Check/D20AttributeCheck'
 import { CheckExecutor } from "./Check/CheckExecutor";
-import { DieModifier } from "./Check/Modifier/DieModifier";
 
-let CE = CheckExecutor.getInstance();
+module.exports = CheckExecutor.getInstance();
 
-let check = new D20AttributeCheck( 13, 10 , 'Strength' );
-check.addModifier( new DieModifier( 'Fire Damage', ['1d6', '2d4'] ) );
-check.addModifier( new DieModifier( 'Fire Resistance', '-1d4' ) );
-CE.execute( check );
-
-console.log( check.report( true ) );
